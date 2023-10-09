0d6b2b9d6bf36490abf9b4787133ff9bcdc2f32a
Jona <=>
2023-10-09 10:21:43 -0600

    Agrega cálculo de cantidad pendiente en dashboard criticos

--- app/Http/Controllers/Dashboards/CriticosController.php
+++ app/Http/Controllers/Dashboards/CriticosController.php
@@ -37,16 +37,25 @@ class CriticosController extends Controller
                 'cantidad' => count($propiedad),
             ]);
 
+            $cantidad_por_ruta = 0;
+
             // Recorre los elementos de cada propiedad
             foreach ($propiedad as $prop) {
+
                 // Calcula la cantidad pendiente: lo que se solicitó menos lo que se surtió
                 $prop->cantidad_pendiente = intval($prop->cantidad_solicitada) - intval($prop->cantidad_surtida);
-                if($prop->cantidad_pendiente < 0 )
-                    $prop->cantidad_pendiente = 0;
 
-                // Agregando los críticos agrupados a una sola lista
-                array_push($mergedData, $prop);
+                if( $prop->cantidad_pendiente >= 0 ) {
+                    // Agregando los críticos agrupados a una sola lista
+                    array_push($mergedData, $prop);
+
+                    // Conador de críticos por ruta
+                    $cantidad_por_ruta = $cantidad_por_ruta +  1;
+
+                }
             }
+            $rutas[count($rutas)-1]['cantidad'] = $cantidad_por_ruta;
+
         }
 
         return response([
